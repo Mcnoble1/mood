7cd03f8ca5f214dd3f2da85f97c65794c515c2f2
mcnoble1 <idowufestustemiloluwa@gmail.com>
2024-05-10 12:02:42 +0100

    feat: added clerk profile widget :octocat:

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 const DashboardLayout = ({ children }) => {
   return (
   <div className="h-screen w-screen relative">
@@ -5,7 +6,11 @@ const DashboardLayout = ({ children }) => {
             Mood
         </aside>
         <div className="ml-[200px]">
-            <header className="h-[60px] border-b border-black/10">Hello</header>
+            <header className="h-[60px] border-b border-black/10">
+                <div className="h-full w-full px-6 flex items-center justify-end">
+                    <UserButton />
+                </div>
+            </header>
             <div>{children}</div>
         </div>
     </div>
